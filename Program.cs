251cc2abab0567d43bb02d75aca92e5ed271c42d
hoanghaisc001 <69134435+hoanghaisc001@users.noreply.github.com>
2024-04-14 21:43:02 +0700

    add code
    add

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int[] a = {1,2,3,4,5,6,7,8};
+
+       Console.WriteLine("Nhap gia tri phan tu can chen");
+
+       int x = Convert.ToInt32(Console.ReadLine());
+       
+       int index = 0;
+       for(int i = 0; i < a.Length ; i++)
+       {
+        if(x == a[i])
+        {
+            index = i;
+        }
+       }
+        
+        //Xoa Phan tu mang
+        
+        for(int i = index; i < a.Length - 1; i++)
+        {
+            a[i] = a[i+1];
+        }
+
+        Console.WriteLine("In gia tri mang ");
+        for(int i =0; i < a.Length ; i++)
+        {
+               Console.Write(a[i] + " , ");
+        }
+      
+
+
+    }
+}
